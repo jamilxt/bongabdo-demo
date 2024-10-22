0fac887b494b5f4423e96c838833182cb8997a08
jamilxt <jamilxt@gmail.com>
2024-10-22 09:13:45 +0600

    init commit

---
+++ src/test/kotlin/com/example/bongabdodemo/BongabdoDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bongabdodemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BongabdoDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
