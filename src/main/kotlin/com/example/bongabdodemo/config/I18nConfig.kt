a81cd7710023d0f1eb6f62f7384917f3461d5c70
jamilxt <jamilxt@gmail.com>
2024-10-22 17:14:26 +0600

    add message source bean for i18n switch fix

--- src/main/kotlin/com/example/bongabdodemo/config/I18nConfig.kt
+++ src/main/kotlin/com/example/bongabdodemo/config/I18nConfig.kt
@@ -1,14 +1,15 @@
 package com.example.bongabdodemo.config
 
+import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.ResourceBundleMessageSource
 import org.springframework.web.servlet.LocaleResolver
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor
 import org.springframework.web.servlet.i18n.SessionLocaleResolver
 import java.util.*
-
 
 @Configuration
 class I18nConfig: WebMvcConfigurer {
@@ -28,5 +29,13 @@ class I18nConfig: WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(localeChangeInterceptor())
+    }
+
+    @Bean
+    fun messageSource(): MessageSource {
+        val messageSource = ResourceBundleMessageSource()
+        messageSource.setBasename("messages")
+        messageSource.setDefaultEncoding("UTF-8")
+        return messageSource
     }
 }
