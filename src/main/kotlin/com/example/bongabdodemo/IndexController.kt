8b37bed8a81594405bb7c58952a31e6b91743096
jamilxt <jamilxt@gmail.com>
2024-10-22 14:23:36 +0600

    add Bongabdo converter

---
+++ src/main/kotlin/com/example/bongabdodemo/IndexController.kt
@@ -0,0 +1,34 @@
+package com.example.bongabdodemo
+
+import com.example.bongabdodemo.utils.BongabdoCalculation.getBanglaAcademyBongabdo
+import com.example.bongabdodemo.utils.BongabdoCalculation.getCalendarFromString
+import com.example.bongabdodemo.utils.BongabdoCalculation.getDateString
+import com.example.bongabdodemo.utils.BongabdoCalculation.getDrikShiddhantaBongabdo
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import java.util.*
+
+@Controller
+@RequestMapping(path = [""])
+class IndexController {
+    @GetMapping
+    fun indexPage(model: Model): String {
+        model.addAttribute("gregorianDate", getDateString(Calendar.getInstance()));
+        return "index"
+    }
+
+    @PostMapping
+    fun convert(@RequestParam("gregorianDate") gregorianDate: String, model: Model): String {
+        model.addAttribute("gregorianDate", gregorianDate);
+
+        val gregorianDateInCalendar = getCalendarFromString(gregorianDate)
+        model.addAttribute("banglaAcademyOutput", getBanglaAcademyBongabdo(gregorianDateInCalendar));
+        model.addAttribute("drikShiddhantaOutput", getDrikShiddhantaBongabdo(gregorianDateInCalendar));
+        model.addAttribute("conversionAvailable", true);
+        return "index";
+    }
+}
---
+++ src/main/kotlin/com/example/bongabdodemo/utils/BongabdoCalculation.kt
@@ -0,0 +1,68 @@
+package com.example.bongabdodemo.utils
+
+import com.hellohasan.bongabdo.api.Bongabdo
+import com.hellohasan.bongabdo.api.BongabdoMethod
+import com.hellohasan.bongabdo.localization_config.BengaliLocalizationConfig
+import com.hellohasan.bongabdo.localization_config.BongabdoLocalizationConfig
+import com.hellohasan.bongabdo.localization_config.EnglishLocalizationConfig
+import org.springframework.context.i18n.LocaleContextHolder
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val DD_MMM_YYYY = "yyyy-MM-dd"
+
+object BongabdoCalculation {
+
+    /**
+     * There are two available calculation methods: [BongabdoMethod.BANGLA_ACADEMY] and [BongabdoMethod.INDIAN_DRIK_SIDDHANTA] available in our library.
+     * But you can extend [Bongabdo] class and implement your own logic inside [getBongabdoData] method.
+     * Example: [SurjaShiddhantaBongabdo]
+     */
+    fun getBanglaAcademyBongabdo(calendar: Calendar): String {
+        val bongabdo = Bongabdo.getInstance(BongabdoMethod.BANGLA_ACADEMY)
+        bongabdo.mLocalizationConfig = getLocalizationConfig()
+        val bongabdoDate = bongabdo.getBongabdoData(
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        return bongabdoDate.getFullDate()
+    }
+
+    fun getDrikShiddhantaBongabdo(calendar: Calendar): String {
+        val bongabdo = Bongabdo.getInstance(BongabdoMethod.INDIAN_DRIK_SIDDHANTA)
+        bongabdo.mLocalizationConfig = getLocalizationConfig()
+        val bongabdoDate = bongabdo.getBongabdoData(
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        return bongabdoDate.getFullDate()
+    }
+
+    /**
+     * You can implement your own localisation config class for Hindi, Arabic or any other language.
+     * Just create a subclass of [BongabdoLocalizationConfig]. Then return it from this method.
+     * Example: [HindiLocalisationConfig]
+     * */
+    private fun getLocalizationConfig(): BongabdoLocalizationConfig {
+        val locale = LocaleContextHolder.getLocale()
+        return if (locale.language == "bn")
+            BengaliLocalizationConfig()
+        else EnglishLocalizationConfig()
+    }
+
+    fun getDateString(calendar: Calendar): String {
+        val formatter = SimpleDateFormat(DD_MMM_YYYY, Locale.getDefault())
+        return formatter.format(calendar.time)
+    }
+
+    fun getCalendarFromString(dateString: String): Calendar {
+        val formatter = SimpleDateFormat(DD_MMM_YYYY, Locale.getDefault())
+        val date = formatter.parse(dateString)
+        return Calendar.getInstance().apply {
+            time = date ?: throw IllegalArgumentException("Invalid date format: $dateString")
+        }
+    }
+
+}
